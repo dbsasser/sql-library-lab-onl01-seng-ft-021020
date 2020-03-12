386c42e3f542e3182bbb6ad0c892535276549ec7
Daniel Sasser <danielbsasser@gmail.com>
2020-03-12 00:16:37 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,32 @@
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id TEXT,
+  sub_genre_id TEXT
+);
+
+CREATE TABLE sub_genres (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER
+);
+
