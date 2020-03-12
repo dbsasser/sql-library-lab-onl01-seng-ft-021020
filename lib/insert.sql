d7c8919471d595fd988799219a8b5d1f4c9a1f72
Daniel Sasser <danielbsasser@gmail.com>
2020-03-12 00:35:19 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -24,20 +24,20 @@ INSERT INTO characters (name, motto, species, author_id) VALUES ('Tom', 'Motto',
 INSERT INTO characters (name, motto, species, author_id) VALUES ('Tom', 'Motto', 'Human', 1);
 INSERT INTO characters (name, motto, species, author_id) VALUES ('Tom', 'Motto', 'Human', 1);
 
-INSERT INTO character_books (character_id, book_id) VALUES (1, 1)
-INSERT INTO character_books (character_id, book_id) VALUES (1, 2)
-INSERT INTO character_books (character_id, book_id) VALUES (1, 3)
-INSERT INTO character_books (character_id, book_id) VALUES (2, 1)
-INSERT INTO character_books (character_id, book_id) VALUES (2, 2)
-INSERT INTO character_books (character_id, book_id) VALUES (2, 3)
-INSERT INTO character_books (character_id, book_id) VALUES (3, 1)
-INSERT INTO character_books (character_id, book_id) VALUES (4, 1)
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 3);
+INSERT INTO character_books (character_id, book_id) VALUES (2, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (2, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (2, 3);
+INSERT INTO character_books (character_id, book_id) VALUES (3, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (4, 1);
 
-INSERT INTO character_books (character_id, book_id) VALUES (5, 4)
-INSERT INTO character_books (character_id, book_id) VALUES (5, 5)
-INSERT INTO character_books (character_id, book_id) VALUES (5, 6)
-INSERT INTO character_books (character_id, book_id) VALUES (6, 4)
-INSERT INTO character_books (character_id, book_id) VALUES (6, 5)
-INSERT INTO character_books (character_id, book_id) VALUES (6, 6)
-INSERT INTO character_books (character_id, book_id) VALUES (7, 4)
-INSERT INTO character_books (character_id, book_id) VALUES (8, 4)
+INSERT INTO character_books (character_id, book_id) VALUES (5, 4);
+INSERT INTO character_books (character_id, book_id) VALUES (5, 5);
+INSERT INTO character_books (character_id, book_id) VALUES (5, 6);
+INSERT INTO character_books (character_id, book_id) VALUES (6, 4);
+INSERT INTO character_books (character_id, book_id) VALUES (6, 5);
+INSERT INTO character_books (character_id, book_id) VALUES (6, 6);
+INSERT INTO character_books (character_id, book_id) VALUES (7, 4);
+INSERT INTO character_books (character_id, book_id) VALUES (8, 4);
